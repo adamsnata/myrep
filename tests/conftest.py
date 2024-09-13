e5f44322c06c92fcebb43e51da48a123c75c6bb8
natal <svetnata74@mail.ru>
2024-09-13 14:46:32 +0200

    allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,26 +14,26 @@ def load_env():
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setting():
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "126.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #
-    #     }
-    # }
-    #
-    # options.capabilities.update(selenoid_capabilities)
-    # selenoid_login = os.getenv("SELENOID_LOGIN")
-    # selenoid_pass = os.getenv("SELENOID_PASS")
-    # selenoid_url = os.getenv("SELENOID_URL")
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     browser.config.base_url = 'https://lamoda.ru'
     browser.config.window_width = 1920
@@ -46,9 +46,9 @@ def browser_setting():
 
     yield
 
-    # attach.add_html(browser)
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_video(browser)
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
 
     browser.quit()
