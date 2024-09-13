10308c1f53e40c77abdd5773a9ad8b8c473707d9
natal <svetnata74@mail.ru>
2024-09-13 13:08:52 +0200

    allure

--- lamoda_tests/web/change_town.py
+++ lamoda_tests/web/change_town.py
@@ -3,32 +3,28 @@ from selene import browser, have, be, by
 
 
 class ChangeTown:
+    def open(self):
+        with allure.step('Открываем главнюу страницу'):
+            browser.open('/')
 
-        def open(self):
-            with allure.step('Открываем главнюу страницу'):
-                browser.open('/')
-
-        @allure.step('Кликаем на стрелочку с названиаем города')
-        def click_geo(self):
+    def click_geo(self):
+        with allure.step('Кликаем на стрелочку с названиаем города'):
             browser.element('._left_s9bjz_34').click()
 
+    def close_pop_up(self):
+        with allure.step('Подтверждаем, что выбираем другой город'):
+            browser.element(by.partial_text('Выбрать')).click()
 
-        def close_pop_up(self):
-            with allure.step('Подтверждаем, что выбираем другой город'):
-                browser.element(by.partial_text('Выбрать')).click()
+    def choose_town(self, value):
+        with allure.step('Выбираем город из списка'):
+            browser.element(by.partial_text(value)).click()
 
+    def choose_button(self):
+        with allure.step('Кликаем кнопку "Перейти"'):
+            browser.element(by.partial_text('Перейти')).click()
 
-        def choose_town(self, value):
-            with allure.step('Выбираем город из списка'):
-                browser.element(by.partial_text(value)).click()
-
-
-        def choose_button(self):
-            with allure.step('Кликаем кнопку "Перейти"'):
-                browser.element(by.partial_text('Перейти')).click()
-
-
-        def should_town(self, value):
-            with allure.step('Проверям, что наша геопозиция изменилась на выбранный '
-                             'город'):
-                browser.element('._left_s9bjz_34').should(have.text(f'г. {value}'))
+    def should_town(self, value):
+        with allure.step(
+                'Проверям, что наша геопозиция изменилась на выбранный '
+                'город'):
+            browser.element('._left_s9bjz_34').should(have.text(f'г. {value}'))
